2bf5f58ce7c28988c664dfb5f3991917f60b98bd
ekayumov <107910871+ekayumov@users.noreply.github.com>
2022-08-06 09:41:36 +0300

    Create l_1.js

---
+++ lesson__6/l_1.js
@@ -0,0 +1,13 @@
+element = document.getElementById('item1')
+myHash = {"title": "sss", "description": "description"}
+
+function parseTemplate(element, myHash) {
+  for(let i = 0; i < element.children.length; i++) {
+    let el = node.children[i]
+    if (el.hasAttribute("data-field")){
+      let text = myHash[el.getAttribute("data-field")]
+      if (text == null){throw Error("error")}
+      el.textContent = text
+    }
+  }
+}
