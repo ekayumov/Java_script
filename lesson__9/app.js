9b42e9aa73deaa63ecda5b770478f8f3a3f64fae
ekayumov <107910871+ekayumov@users.noreply.github.com>
2022-08-25 03:08:49 +0300

    Create app.js

---
+++ lesson__9/app.js
@@ -0,0 +1,41 @@
+const API_KEY = "i3cMVYkFVKlY2qn7wfbmgIRQNGBj1roI"
+let queryCache = {};
+let input = document.getElementById("searchInput")
+let queryTimeoutID = undefined
+
+function giphyQuery(query) {
+  let getUrl = `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${API_KEY}`
+    if (query === "") return
+    if (queryCache[query]) {
+      return queryCache[query]
+    }
+    else {
+  		return fetch(giphyUrl)
+  			.then((response) => response.json())
+  			.then((json) => json.data)
+  			.then((results) => {
+  				queryCache[query] = results
+  				return results
+  			})
+  	}
+}
+
+function updatePage(myInput, results) {
+	myInput.textContent = ''
+	results.forEach((gif) => {
+		let item = document.createElement('img')
+		item.setAttribute('src', gif.images.fixed_height_small.url)
+		myInput.append(item)
+	})
+
+	return results
+}
+
+input.addEventListener('input', (event) => {
+	clearTimeout(queryTimeoutID)
+
+	let gifResults = document.querySelector('.results')
+	queryTimeoutID = setTimeout((query) => {
+		giphyQuery(query).then((results) => updatePage(gifResults, results))
+	}, 500, event.target.value)
+})
