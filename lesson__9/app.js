031666181a10c28c710abcb44f9c4381f38dc6c1
ekayumov <107910871+ekayumov@users.noreply.github.com>
2022-08-25 13:45:29 +0300

    Update app.js

--- lesson__9/app.js
+++ lesson__9/app.js
@@ -1,29 +1,29 @@
 const API_KEY = "ByZKDsBLbrcV7KuitjfprBUt7kJ4nkXy"
 let queryCache = {};
-let input = document.getElementById("searchInput")
+const input = document.getElementById("searchInput")
 let queryTimeoutID = undefined
 
 function giphyQuery(query) {
-  let getUrl = `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${API_KEY}`
-    if (query === "") return
-    if (queryCache[query]) {
-      return queryCache[query]
-    }
-    else {
-  		return fetch(giphyUrl)
-  			.then((response) => response.json())
-  			.then((json) => json.data)
-  			.then((results) => {
-  				queryCache[query] = results
-  				return results
-  			})
-  	}
+  const getUrl = `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${API_KEY}`
+  if (query === "") return
+  if (queryCache[query]) {
+    return queryCache[query]
+  }
+  else {
+    return fetch(giphyUrl)
+  	.then((response) => response.json())
+  	.then((json) => json.data)
+  	.then((results) => {
+  		queryCache[query] = results
+  		 return results
+  	})
+  }
 }
 
 function updatePage(myInput, results) {
 	myInput.textContent = ''
 	results.forEach((gif) => {
-		let item = document.createElement('img')
+		const item = document.createElement('img')
 		item.setAttribute('src', gif.images.fixed_height_small.url)
 		myInput.append(item)
 	})
@@ -31,11 +31,15 @@ function updatePage(myInput, results) {
 	return results
 }
 
+function queryTimeout(gifRes, event) {
+  queryTimeoutID = setTimeout((query) => {
+		giphyQuery(query).then((results) => updatePage(gifRes, results))
+	}, 500, event.target.value)
+
+}
+
 input.addEventListener('input', (event) => {
 	clearTimeout(queryTimeoutID)
-
-	let gifResults = document.querySelector('.results')
-	queryTimeoutID = setTimeout((query) => {
-		giphyQuery(query).then((results) => updatePage(gifResults, results))
-	}, 500, event.target.value)
+	const gifResults = document.querySelector('.results')
+	queryTimeout(gifResults, event)
 })
