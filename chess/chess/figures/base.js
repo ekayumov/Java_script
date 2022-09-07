dd994dc1d28e392d7b3d1024f185df69f7448cd4
ekayumov <107910871+ekayumov@users.noreply.github.com>
2022-09-07 04:26:35 +0300

    add figures

---
+++ chess/chess/figures/base.js
@@ -0,0 +1,13 @@
+export default class BaseFigure {
+    constructor(color, desk, cell) {
+        this.color = color
+        this.desk = desk
+        this.cell = cell
+    }
+
+    get color() { return this.color }
+    get desk() { return this.desk }
+    get cell() { return this.cell }
+
+    moves() {}
+}
